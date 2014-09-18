aab70d46d148b597c8398c4db3e7a35fe1c3a42c
Anton Warnhag <anton.warnhag@gmail.com>
2014-09-18 18:43:19 +0200

    Added support for Novint Flacon (for real this time).

---
+++ Assets/FalconInputInterface.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FalconInputInterface : InputInterface {
+
+	public int falconNum;
+	Vector3 tipPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FalconUnity.getTipPosition (falconNum, out tipPosition)) {
+			accelerate = Mathf.Clamp(((tipPosition.z - 0.035f) * -1 * 10), 0, 1);
+		} else {
+			accelerate = 0;
+		}
+	}
+
+}
